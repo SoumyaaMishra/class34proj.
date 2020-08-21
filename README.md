97fa74888752950d092b9332cc01553c12d47047
SoumyaaMishra <66158317+SoumyaaMishra@users.noreply.github.com>
2020-08-21 07:44:36 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# class34proj.
+# asynchronousBallMovement
+Boiler plate for ball moving aysnchronously
---
+++ sketch.js
@@ -0,0 +1,44 @@
+var hypball;
+var database, position;
+function setup(){
+    createCanvas(500,500);
+    database = firebase.database();
+    hypball = createSprite(250,250,10,10);
+    hypball.shapeColor = "red";
+    var ballPosition = database.ref('ball/position');
+        ballPosition.on('value', readPosition, showERR);
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function showERR(){
+    console.log("error");
+}
+
+function readPosition(data){
+    position = data.val();
+    hypball.x = position.x;
+    hypball.y = position.y;
+}
+
+function writePosition(x,y){
+    database.ref('ball/position').set({
+        'x': x+position.x,
+        'y': y+position.y
+    })
+}
